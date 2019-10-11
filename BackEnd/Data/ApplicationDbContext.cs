285b6eed96718ca9884ad7b4989177067d73c847
Jason <jsykim33@gmail.com>
2019-10-11 13:49:53 -0400

    updated db context to account for new models added

--- BackEnd/Data/ApplicationDbContext.cs
+++ BackEnd/Data/ApplicationDbContext.cs
@@ -1,18 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace BackEnd.Models
+namespace BackEnd.Data
 {
-    // this class is defining how we interact with the database
-    // this class inherits the DbContext class
-    /// <summary> This is the connection object for our database </summary>
     public class ApplicationDbContext : DbContext
     {
-        // DbContextOptions: configurations that tells this class how we connect to the db
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
         {
 
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendee>()
+                .HasIndex(a => a.UserName)
+                .IsUnique();
+
+            // Ignore the computed property
+            modelBuilder.Entity<Session>()
+                 .Ignore(s => s.Duration);
+
+            // Many-to-many: Conference <-> Attendee
+            modelBuilder.Entity<ConferenceAttendee>()
+                .HasKey(ca => new { ca.ConferenceID, ca.AttendeeID });
+
+            // Many-to-many: Session <-> Attendee
+            modelBuilder.Entity<SessionAttendee>()
+                .HasKey(ca => new { ca.SessionID, ca.AttendeeID });
+
+            // Many-to-many: Speaker <-> Session
+            modelBuilder.Entity<SessionSpeaker>()
+               .HasKey(ss => new { ss.SessionId, ss.SpeakerId });
+
+            // Many-to-many: Session <-> Tag
+            modelBuilder.Entity<SessionTag>()
+                .HasKey(st => new { st.SessionID, st.TagID });
+        }
+
+        public DbSet<Conference> Conferences { get; set; }
+
+        public DbSet<Session> Sessions { get; set; }
+
+        public DbSet<Track> Tracks { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+
         public DbSet<Speaker> Speakers { get; set; }
+
+        public DbSet<Attendee> Attendees { get; set; }
     }
 }
